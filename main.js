4800ab10d146dbcdac7c4a7a5667f56ae81ab901
Sean Currlin <sean.currlin@gmail.com>
2025-09-12 02:01:50 -0400

    final responsiveness updates

--- main.js
+++ main.js
@@ -12,16 +12,8 @@ function initTableauViz(vizId) {
     const divElement = document.getElementById(vizId);
     if (divElement) {
         const vizElement = divElement.getElementsByTagName('object')[0];
-        if (divElement.offsetWidth > 800) { 
-            vizElement.style.width = '100%';
-            vizElement.style.height = '627px';
-        } else if (divElement.offsetWidth > 500) { 
-            vizElement.style.width = '100%';
-            vizElement.style.height = '627px';
-        } else { 
-            vizElement.style.width = '100%';
-            vizElement.style.height = '727px';
-        }
+        vizElement.style.width = '800px';
+        vizElement.style.height = '627px';
         
         const scriptElement = document.createElement('script');
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
